a0666935ecddcbd054e2a5c6b4519471f3e20c99
Alex Bilbie <hello@alexbilbie.com>
2013-03-15 11:35:19 +0000

    Added Provider\User class

---
+++ src/OAuth2/Client/Provider/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OAuth2\Client\Provider;
+
+class User {
+
+    protected $uid = null;
+    protected $nickname = null;
+    protected $name = null;
+    protected $firstName = null;
+    protected $lastName = null;
+    protected $email = null;
+    protected $location = null;
+    protected $description = null;
+    protected $imageUrl = null;
+    protected $urls = null;
+
+    public function __set($name, $value)
+    {
+        if (isset($this->{$name})) {
+            $this->{$name} = $value;
+        }
+    }
+
+    public function __get($name)
+    {
+        if (isset($this->{$name})) {
+            return $this->{$name};
+        } else {
+            return null;
+        }
+    }
+
+}
+
