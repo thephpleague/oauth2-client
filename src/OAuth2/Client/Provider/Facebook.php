36342b5fb7ce55a039a7286ce1f6958732f0e83f
Alex Bilbie <hello@alexbilbie.com>
2013-03-15 11:37:46 +0000

    Little fixes for Facebook controller

--- src/OAuth2/Client/Provider/Facebook.php
+++ src/OAuth2/Client/Provider/Facebook.php
@@ -25,16 +25,16 @@ class Facebook extends IdentityProvider
     {
         $imageHeaders = get_headers('https://graph.facebook.com/me/picture?type=normal&access_token='.$token->accessToken, 1);
 
-        $user = new Provder\User;
+        $user = new User;
         $user->uid = $response->id;
         $user->nickname = $response->username;
         $user->name = $response->name;
-        $user->first_name = $response->first_name;
-        $user->last_name = $response->last_name;
+        $user->firstName = $response->first_name;
+        $user->lastName = $response->last_name;
         $user->email = isset($response->email) ? $response->email : null;
         $user->location = isset($response->hometown->name) ? $response->hometown->name : null;
         $user->description = isset($response->bio) ? $response->bio : null;
-        $user->image = $imageHeaders['Location'];
+        $user->imageUrl = $imageHeaders['Location'];
         $user->urls = array(
             'Facebook' => $response->link,
         );
