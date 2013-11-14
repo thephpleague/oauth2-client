cf585467efde9a9bfd34a7d2435fd42fc9b64aa5
pfchen <pfchen31@gmail.com>
2013-11-14 18:55:54 +0800

    add Httpclient folder

---
+++ src/League/OAuth2/Client/HttpClient/HttpClientInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace League\OAuth2\Client\HttpClient;
+
+/**
+ * HttpClientInterface
+ *
+ * 
+ *
+ */
+interface HttpClientInterface {
+
+    public function get();
+
+    public function post();
+}
