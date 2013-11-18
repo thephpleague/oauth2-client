cd58782059b641af542b7a240ec92cc8f4807080
Phil Sturgeon <email@philsturgeon.co.uk>
2013-11-18 14:19:24 -0500

    Add a fake pass for now.

---
+++ tests/ProviderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class ProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testPoorExcuses()
+    {
+        // We clearly need to write some unit-tests for this
+        $this->assertTrue(true);
+    }
+}
