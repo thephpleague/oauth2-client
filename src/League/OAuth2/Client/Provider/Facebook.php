2a74afb16703c107e9eb8ef4568d6076ec947533
Robert Pitt <robertpitt1988@gmail.com>
2014-01-17 23:31:06 +0000

    Commented Facebook provider
    Added Comments for the Facebook Provider class.

--- src/League/OAuth2/Client/Provider/Facebook.php
+++ src/League/OAuth2/Client/Provider/Facebook.php
@@ -4,24 +4,51 @@ namespace League\OAuth2\Client\Provider;
 
 class Facebook extends IdentityProvider
 {
+    /**
+     * Required Facebook scopes.
+     * @var array
+     */
     public $scopes = array('offline_access', 'email', 'read_stream');
+
+    /**
+     * Response type
+     * @var string
+     */
     public $responseType = 'string';
 
+    /**
+     * Facebooks authorization endpoint
+     * @return String
+     */
     public function urlAuthorize()
     {
         return 'https://www.facebook.com/dialog/oauth';
     }
 
+    /**
+     * Facebooks access token endpoint
+     * @return String
+     */
     public function urlAccessToken()
     {
         return 'https://graph.facebook.com/oauth/access_token';
     }
 
+    /**
+     * Get the authorized url for the facebook user data
+     * @return String
+     */
     public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
     {
         return 'https://graph.facebook.com/me?access_token='.$token;
     }
 
+    /**
+     * Construct a User object with the response
+     * @param  stdClass                               $response Response frm the graph server
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return League\OAuth2\Client\Provider\User               User Object
+     */
     public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         $imageHeaders = get_headers('https://graph.facebook.com/me/picture?type=normal&access_token='.$token->accessToken, 1);
@@ -43,16 +70,34 @@ class Facebook extends IdentityProvider
         return $user;
     }
 
+    /**
+     * Return the User ID from the response object
+     * @param  stdClass                               $response Response from the Facebook graph
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return String                                           Facebook User ID
+     */
     public function userUid($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return $response->id;
     }
 
+    /**
+     * Return the Email from the response object
+     * @param  stdClass                               $response Response from the Facebook graph
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return String                                           Email address
+     */
     public function userEmail($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return isset($response->email) && $response->email ? $response->email : null;
     }
 
+    /**
+     * Return the screen name from the response object
+     * @param  stdClass                               $response Response from the Facebook graph
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return Array                                           Firstname, Lastname
+     */
     public function userScreenName($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return array($response->first_name, $response->last_name);
