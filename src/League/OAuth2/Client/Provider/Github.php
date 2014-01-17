f9a59bba41c19ad1aebb99f5ee8dc8f6f34e7e93
Robert Pitt <robertpitt1988@gmail.com>
2014-01-17 23:36:53 +0000

    Code Commenting
    Commented Github class.

--- src/League/OAuth2/Client/Provider/Github.php
+++ src/League/OAuth2/Client/Provider/Github.php
@@ -4,23 +4,46 @@ namespace League\OAuth2\Client\Provider;
 
 class Github extends IdentityProvider
 {
+    /**
+     * Response Type
+     * @var string
+     */
     public $responseType = 'string';
 
+    /**
+     * Githubs Authorization url
+     * @return String
+     */
     public function urlAuthorize()
     {
         return 'https://github.com/login/oauth/authorize';
     }
 
+    /**
+     * Githubs Access Token url
+     * @return String
+     */
     public function urlAccessToken()
     {
         return 'https://github.com/login/oauth/access_token';
     }
 
+    /**
+     * Get the authorized url for the githubs user data
+     * @param  League\OAuth2\Client\Token\AccessToken $token Token Object
+     * @return String                                        Authorized url
+     */
     public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
     {
         return 'https://api.github.com/user?access_token='.$token;
     }
 
+    /**
+     * Construct a User object with the response
+     * @param  stdClass                               $response Response from the api server
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return League\OAuth2\Client\Provider\User               User Object
+     */
     public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         $user = new User;
@@ -36,16 +59,34 @@ class Github extends IdentityProvider
         return $user;
     }
 
+    /**
+     * Return the User ID from the response object
+     * @param  stdClass                               $response Response from the API
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return String                                           Github User ID
+     */
     public function userUid($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return $response->id;
     }
 
+    /**
+     * Return the Email from the response object
+     * @param  stdClass                               $response Response from the API
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return String                                           Email address
+     */
     public function userEmail($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return isset($response->email) && $response->email ? $response->email : null;
     }
 
+    /**
+     * Return the screen name from the response object
+     * @param  stdClass                               $response Response from the API
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return String                                           Screename
+     */
     public function userScreenName($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return $response->name;
