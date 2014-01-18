b885d711a56f13d476a8c1f0e8da71ee954dcd47
Robert Pitt <robertpitt1988@gmail.com>
2014-01-18 00:17:10 +0000

    Commented Vkontakte class.

--- src/League/OAuth2/Client/Provider/Vkontakte.php
+++ src/League/OAuth2/Client/Provider/Vkontakte.php
@@ -4,50 +4,85 @@ namespace League\OAuth2\Client\Provider;
 
 class Vkontakte extends IdentityProvider
 {
+    /**
+     * Required Vkontakte scopes
+     * @var array
+     */
     public $scopes = array();
+
+    /**
+     * Response Type
+     * @var string
+     */
     public $responseType = 'json';
 
+    /**
+     * Request fields (properties that we want from Vkontakte's API)
+     * @var array
+     */
+    public $fields = array(
+        'nickname',
+        'screen_name',
+        'sex',
+        'bdate',
+        'city',
+        'country',
+        'timezone',
+        'photo_50',
+        'photo_100',
+        'photo_200_orig',
+        'has_mobile',
+        'contacts',
+        'education',
+        'online',
+        'counters',
+        'relation',
+        'last_seen',
+        'status',
+        'can_write_private_message',
+        'can_see_all_posts',
+        'can_see_audio',
+        'can_post',
+        'universities',
+        'schools',
+        'verified'
+    );
+
+    /**
+     * Vkontakte's Auhtorization endpoint
+     * @return String
+     */
     public function urlAuthorize()
     {
         return 'https://oauth.vk.com/authorize';
     }
 
+    /**
+     * Vkontakte's Access Token endpoint
+     * @return String
+     */
     public function urlAccessToken()
     {
         return 'https://oauth.vk.com/access_token';
     }
 
+    /**
+     * Authorized endpoint for user data
+     * @param  League\OAuth2\Clien\tToken\AccessToken $token Access Token
+     * @return String
+     */
     public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
     {
-        $fields = array('nickname',
-            'screen_name',
-            'sex',
-            'bdate',
-            'city',
-            'country',
-            'timezone',
-            'photo_50',
-            'photo_100',
-            'photo_200_orig',
-            'has_mobile',
-            'contacts',
-            'education',
-            'online',
-            'counters',
-            'relation',
-            'last_seen',
-            'status',
-            'can_write_private_message',
-            'can_see_all_posts',
-            'can_see_audio',
-            'can_post',
-            'universities',
-            'schools',
-            'verified');
-
-        return "https://api.vk.com/method/users.get?user_id={$token->uid}&fields=".implode(",", $fields)."&access_token=".$token;
+        return "https://api.vk.com/method/users.get?user_id={$token->uid}&fields=".implode(",", $this->fields)."&access_token=".$token;
     }
 
+
+    /**
+     * Construct a User object with the response
+     * @param  stdClass                               $response Response from the API Server
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return League\OAuth2\Client\Provider\User               User Object
+     */
     public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         $response = $response->response[0];
@@ -66,6 +101,12 @@ class Vkontakte extends IdentityProvider
         return $user;
     }
 
+    /**
+     * Return the User ID from the response object
+     * @param  stdClass                               $response Response from the API Server
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return String                                           Vkontakte's User ID
+     */
     public function userUid($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         $response = $response->response[0];
@@ -73,6 +114,12 @@ class Vkontakte extends IdentityProvider
         return $response->uid;
     }
 
+    /**
+     * Return the Email from the response object
+     * @param  stdClass                               $response Response from the API Server
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return String                                           Email address
+     */
     public function userEmail($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         $response = $response->response[0];
@@ -80,6 +127,12 @@ class Vkontakte extends IdentityProvider
         return isset($response->email) && $response->email ? $response->email : null;
     }
 
+    /**
+     * Return the screen name from the response object
+     * @param  stdClass                               $response Response from the API Server
+     * @param  League\OAuth2\Client\Token\AccessToken $token    Token Object
+     * @return Array                                            Firstname, Lastname
+     */
     public function userScreenName($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         $response = $response->response[0];
