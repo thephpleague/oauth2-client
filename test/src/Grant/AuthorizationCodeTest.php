0237107a628929db7d51fbd717d98cab76b02804
Jildert Miedema <jildert@pimenta.nl>
2014-12-01 08:03:06 +0100

    remove unused assertions

--- test/src/Grant/AuthorizationCodeTest.php
+++ test/src/Grant/AuthorizationCodeTest.php
@@ -34,14 +34,6 @@ class AuthorizationCodeTest extends \PHPUnit_Framework_TestCase
      */
     public function testInvalidRefreshToken()
     {
-        $response = m::mock('Guzzle\Http\Message\Response');
-        $response->shouldReceive('getBody')->times(0)->andReturn('{"access_token": "mock_access_token", "expires": 3600, "refresh_token": "mock_refresh_token", "uid": 1}');
-
-        $client = m::mock('Guzzle\Service\Client');
-        $client->shouldReceive('setBaseUrl')->times(0);
-        $client->shouldReceive('post->send')->times(0)->andReturn($response);
-        $this->provider->setHttpClient($client);
-
         $this->provider->getAccessToken('authorization_code', ['invalid_code' => 'mock_authorization_code']);
     }
 }
