7446829c3b86a8a162f3c06718718c608b78a730
Woody Gilk <woody.gilk@gmail.com>
2015-02-23 12:14:17 -0600

    replace custom code in AccessToken with Vkontakte::$uidKey

--- src/Token/AccessToken.php
+++ src/Token/AccessToken.php
@@ -46,9 +46,6 @@ class AccessToken
         // Some providers (not many) give the uid here, so lets take it
         isset($options['uid']) and $this->uid = $options['uid'];
 
-        // Vkontakte uses user_id instead of uid
-        isset($options['user_id']) and $this->uid = $options['user_id'];
-
         // Mailru uses x_mailru_vid instead of uid
         isset($options['x_mailru_vid']) and $this->uid = $options['x_mailru_vid'];
 
