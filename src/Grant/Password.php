b242e1f8f4f1f87e0d751d93c55df7f24f543065
Woody Gilk <woody.gilk@gmail.com>
2015-05-01 23:18:11 -0500

    Use AbstractGrant for default grants

--- src/Grant/Password.php
+++ src/Grant/Password.php
@@ -2,32 +2,18 @@
 
 namespace League\OAuth2\Client\Grant;
 
-use League\OAuth2\Client\Token\AccessToken;
-
-class Password implements GrantInterface
+class Password extends AbstractGrant
 {
     public function __toString()
     {
         return 'password';
     }
 
-    public function prepRequestParams($defaultParams, $params)
+    protected function getRequiredRequestParams()
     {
-        if (! isset($params['username']) || empty($params['username'])) {
-            throw new \BadMethodCallException('Missing username');
-        }
-
-        if (! isset($params['password']) || empty($params['password'])) {
-            throw new \BadMethodCallException('Missing password');
-        }
-
-        $params['grant_type'] = 'password';
-
-        return array_merge($defaultParams, $params);
-    }
-
-    public function handleResponse($response = array())
-    {
-        return new AccessToken($response);
+        return [
+            'username',
+            'password',
+        ];
     }
 }
