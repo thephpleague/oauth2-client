9dfdf8ec105e11bbe79d13302c6712459ee08260
Woody Gilk <woody.gilk@gmail.com>
2015-05-24 23:34:17 -0500

    Add StandardProvider as a configurable provider
    Allows creating providers without having to implement a custom provider class.
    
    Fixes #179

---
+++ src/Provider/StandardProvider.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace League\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\ResponseInterface;
+use InvalidArgumentException;
+
+class StandardProvider extends AbstractProvider
+{
+    /**
+     * @var string
+     */
+    private $urlAuthorize;
+
+    /**
+     * @var string
+     */
+    private $urlAccessToken;
+
+    /**
+     * @var string
+     */
+    private $urlUserDetails;
+
+    /**
+     * @var string
+     */
+    private $accessTokenMethod;
+
+    /**
+     * @var string
+     */
+    private $accessTokenUid;
+
+    /**
+     * @var array|null
+     */
+    private $scopes = null;
+
+    /**
+     * @var string
+     */
+    private $scopeSeparator;
+
+    /**
+     * @var string
+     */
+    private $responseError = 'error';
+
+    /**
+     * @var string
+     */
+    private $responseCode;
+
+    /**
+     * @var string
+     */
+    private $responseUid = 'id';
+
+    public function __construct($options = [], array $collaborators = [])
+    {
+        $this->assertRequiredOptions($options);
+
+        $possible   = $this->getConfigurableOptions();
+        $configured = array_intersect_key($options, array_flip($possible));
+
+        foreach ($configured as $key => $value) {
+            $this->$key = $value;
+        }
+
+        // Remove all options that are only used locally
+        $options = array_diff_key($options, $configured);
+
+        return parent::__construct($options, $collaborators);
+    }
+
+    /**
+     * Get all options that can be configured.
+     *
+     * @return array
+     */
+    protected function getConfigurableOptions()
+    {
+        return array_merge($this->getRequiredOptions(), [
+            'accessTokenMethod',
+            'accessTokenUid',
+            'scopeSeparator',
+            'responseError',
+            'responseCode',
+            'responseUid',
+            'scopes',
+        ]);
+    }
+
+    /**
+     * Get all options that are required.
+     *
+     * @return array
+     */
+    protected function getRequiredOptions()
+    {
+        return [
+            'urlAuthorize',
+            'urlAccessToken',
+            'urlUserDetails',
+        ];
+    }
+
+    /**
+     * Verify that all required options have been passed.
+     *
+     * @param  array $options
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    private function assertRequiredOptions(array $options)
+    {
+        $missing = array_diff_key(array_flip($this->getRequiredOptions()), $options);
+        if ($missing) {
+            throw new InvalidArgumentException(
+                'Required options not defined: ' . implode(', ', array_keys($missing))
+            );
+        }
+    }
+
+    public function getBaseAuthorizationUrl()
+    {
+        return $this->urlAuthorize;
+    }
+
+    public function getBaseAccessTokenUrl()
+    {
+        return $this->urlAuthorize;
+    }
+
+    public function getUserDetailsUrl(AccessToken $token)
+    {
+        return $this->urlUserDetails;
+    }
+
+    public function getDefaultScopes()
+    {
+        return $this->scopes;
+    }
+
+    protected function getAccessTokenMethod()
+    {
+        return $this->accessTokenMethod ?: parent::getAccessTokenMethod();
+    }
+
+    protected function getAccessTokenUid()
+    {
+        return $this->accessTokenUid ?: parent::getAccessTokenUid();
+    }
+
+    protected function getScopeSeparator()
+    {
+        return $this->scopeSeparator ?: parent::getScopeSeparator();
+    }
+
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (!empty($data[$this->responseError])) {
+            $error = $data[$this->responseError];
+            $code  = $this->responseCode ? $data[$this->responseCode] : 0;
+            throw new IdentityProviderException($error, $code, $data);
+        }
+    }
+
+    protected function prepareUserDetails(array $response, AccessToken $token)
+    {
+        return new StandardUser($response, $this->responseUid);
+    }
+}
