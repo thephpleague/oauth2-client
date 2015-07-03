46fbf2e8ddcfe3581ce4cdc6ea0b69a2eb9e8151
Ben Ramsey <ben@benramsey.com>
2015-07-03 09:50:27 -0500

    Adding docblock file header to each source file
    Part of the work required for #351

--- src/Grant/AbstractGrant.php
+++ src/Grant/AbstractGrant.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This file is part of the league/oauth2-client library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Alex Bilbie <hello@alexbilbie.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link http://thephpleague.com/oauth2-client/ Documentation
+ * @link https://packagist.org/packages/league/oauth2-client Packagist
+ * @link https://github.com/thephpleague/oauth2-client GitHub
+ */
 
 namespace League\OAuth2\Client\Grant;
 
