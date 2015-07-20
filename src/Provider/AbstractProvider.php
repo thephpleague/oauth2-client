eaf31dac51265bbbb83874bea13b2849db2fa345
Steven Maguire <stevenmaguire@gmail.com>
2015-07-20 11:00:16 -0500

    add dot notation support to access token resource owner id

--- src/Provider/AbstractProvider.php
+++ src/Provider/AbstractProvider.php
@@ -657,7 +657,10 @@ abstract class AbstractProvider
     protected function prepareAccessTokenResponse(array $result)
     {
         if ($this->getAccessTokenResourceOwnerId()) {
-            $result['resource_owner_id'] = $result[$this->getAccessTokenResourceOwnerId()];
+            $result['resource_owner_id'] = $this->getValueByKey(
+                $this->getAccessTokenResourceOwnerId(),
+                $result
+            );
         }
         return $result;
     }
@@ -748,5 +751,37 @@ abstract class AbstractProvider
         }
 
         return $this->getDefaultHeaders();
+    }
+
+    /**
+     * Get value by key with dot notation.
+     *
+     * @param  string  $key
+     * @param  array   $data
+     * @param  mixed   $default Optional
+     *
+     * @return mixed
+     */
+    protected function getValueByKey($key, array $data, $default = null)
+    {
+        if (!is_string($key) || empty($key) || !count($data)) {
+            return $default;
+        }
+
+        if (strpos($key, '.') !== false) {
+            $keys = explode('.', $key);
+
+            foreach ($keys as $innerKey) {
+                if (!array_key_exists($innerKey, $data)) {
+                    return $default;
+                }
+
+                $data = $data[$innerKey];
+            }
+
+            return $data;
+        }
+
+        return array_key_exists($key, $data) ? $data[$key] : $default;
     }
 }
