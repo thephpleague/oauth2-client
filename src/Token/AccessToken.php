5d5093cc18d091da0a8ae63b84c47c0402572506
Woody Gilk <woody.gilk@gmail.com>
2016-04-18 08:04:00 -0500

    Add support for capturing access token values
    Vendors (such as Google) sometimes pass additional information with
    access token responses. Capturing these values allows package users to
    make use of them when appropriate.
    
    Refs thephpleague/oauth2-google#14

--- src/Token/AccessToken.php
+++ src/Token/AccessToken.php
@@ -46,6 +46,11 @@ class AccessToken implements JsonSerializable
     protected $resourceOwnerId;
 
     /**
+     * @var array
+     */
+    protected $values = [];
+
+    /**
      * Constructs an access token.
      *
      * @param array $options An array of options returned by the service provider
@@ -84,6 +89,17 @@ class AccessToken implements JsonSerializable
 
             $this->expires = $expires;
         }
+
+        // Capure any additional values that might exist in the token but are
+        // not part of the standard response. Vendors will sometimes pass
+        // additional user data this way.
+        $this->values = array_diff_key($options, array_flip([
+            'access_token',
+            'resource_owner_id',
+            'refresh_token',
+            'expires_in',
+            'expires',
+        ]));
     }
 
     /**
@@ -158,6 +174,16 @@ class AccessToken implements JsonSerializable
     }
 
     /**
+     * Returns additional vendor values stored in the token.
+     *
+     * @return array
+     */
+    public function getValues()
+    {
+        return $this->values;
+    }
+
+    /**
      * Returns the token key.
      *
      * @return string
@@ -175,7 +201,7 @@ class AccessToken implements JsonSerializable
      */
     public function jsonSerialize()
     {
-        $parameters = [];
+        $parameters = $this->values;
 
         if ($this->accessToken) {
             $parameters['access_token'] = $this->accessToken;
