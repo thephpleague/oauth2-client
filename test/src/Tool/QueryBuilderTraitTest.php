addfc24bbaa5567982afdf1c40f01283c88ac286
Woody Gilk <woody.gilk@gmail.com>
2016-04-29 10:04:57 -0500

    Ensure generated query strings are consistent
    PHP configuration can be modified to set a different argument
    separator from the standard `&`. Generated query strings should always
    use the standard separator.
    
    Fixes #519

---
+++ test/src/Tool/QueryBuilderTraitTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace League\OAuth2\Client\Test\Tool;
+
+use League\OAuth2\Client\Tool\QueryBuilderTrait;
+use PHPUnit_Framework_TestCase;
+
+class QueryBuilderTraitTest extends PHPUnit_Framework_TestCase
+{
+    use QueryBuilderTrait;
+
+    public function setUp()
+    {
+        ini_set('arg_separator.output', '&amp;');
+    }
+
+    public function testBuildQueryString()
+    {
+        $params = [
+            'a' => 'foo',
+            'b' => 'bar',
+        ];
+
+        $query = $this->buildQueryString($params);
+
+        $this->assertSame('a=foo&b=bar', $query);
+    }
+}
