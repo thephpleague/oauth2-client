c916abbd7bd52492f76f8179f08a2fcab59bb19e
John Noel <chaostangent@gmail.com>
2016-07-28 10:18:00 +0100

    Update testRandomGeneratorCreatesRandomState test
    Changes the Generator mock to expect a character set integer (in this
    case 7 which is the Generator::CHAR_ALNUM constant).

--- test/src/Provider/AbstractProviderTest.php
+++ test/src/Provider/AbstractProviderTest.php
@@ -64,7 +64,7 @@ class AbstractProviderTest extends \PHPUnit_Framework_TestCase
             'state' => 'XXX'
         ]));
     }
-    
+
     /**
      * Tests https://github.com/thephpleague/oauth2-client/pull/485
      */
@@ -75,7 +75,7 @@ class AbstractProviderTest extends \PHPUnit_Framework_TestCase
         ]);
         $query = parse_url($url, PHP_URL_QUERY);
         $this->assertNotEmpty($query);
-        
+
         parse_str($query, $params);
         $this->assertArrayHasKey('foo', $params);
         $this->assertSame('BAR', $params['foo']);
@@ -307,7 +307,7 @@ class AbstractProviderTest extends \PHPUnit_Framework_TestCase
         $xstate = str_repeat('x', 32);
 
         $generator = m::mock(RandomGenerator::class);
-        $generator->shouldReceive('generateString')->with(32)->times(1)->andReturn($xstate);
+        $generator->shouldReceive('generateString')->with(32, 7)->times(1)->andReturn($xstate);
 
         $factory = m::mock(RandomFactory::class);
         $factory->shouldReceive('getMediumStrengthGenerator')->times(1)->andReturn($generator);
