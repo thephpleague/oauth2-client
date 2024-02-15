604dcf96f6a847c1b3eb5e1926c96385eecefd3b
Korvin Szanto <Korvinszanto@gmail.com>
2024-02-15 10:03:28 -0800

    Update dockblock for AbstractProvider::prepareAccessTokenResponse
    This commit updates the docblock to reflect the expected type for $result.

--- src/Provider/AbstractProvider.php
+++ src/Provider/AbstractProvider.php
@@ -815,7 +815,7 @@ abstract class AbstractProvider
      * Custom mapping of expiration, etc should be done here. Always call the
      * parent method when overloading this method.
      *
-     * @param  mixed $result
+     * @param  array<string, mixed> $result
      * @return array
      */
     protected function prepareAccessTokenResponse(array $result)
